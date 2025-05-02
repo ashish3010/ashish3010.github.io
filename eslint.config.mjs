076c0d37396200c3d5eb4156c1f26b4048b49d5c
Ashish42.Mishra <ashish42.mishra@ril.com>
2025-05-02 18:47:44 +0530

    added lint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,13 @@ const eslintConfig = [
     rules: {
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-explicit-any": "warn"
+      "@typescript-eslint/no-explicit-any": "warn",
     },
+    ignores: [
+      ".next",
+      "node_modules",
+      "package-lock.json",
+    ],
   },
 ];
 
