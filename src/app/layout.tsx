8a4f1d76206e19e0398ecfbeb010a8c1ae79b340
Ashish42.Mishra <ashish42.mishra@ril.com>
2025-05-02 18:11:26 +0530

    updated site url in meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import GoogleAnalytics from './google-analytics';
 
 const title = 'Ashish Mishra | Frontend Developer From Mumbai, India.';
 const description = 'Frontend developer specializing in modern, responsive web interfaces with sleek animations and a focus on user experience.';
-const url = 'https://ashish3010.github.io/';
+const url = 'https://ashishmishra.vercel.app/';
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
